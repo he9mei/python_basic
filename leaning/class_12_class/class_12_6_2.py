92e520dc911439d0332e2ce254ab7b0d61071ac0
he9mei <396167189@qq.com>
2019-11-25 20:35:47 +0800

    构造方法

--- leaning/class_12_class/class_12_6_2.py
+++ leaning/class_12_class/class_12_6_2.py
@@ -10,7 +10,7 @@
 # （4）如何获取私有变量？get_attrs方法
 # （5）如果修改私有变量？set_attrs方法
 # 在python中，通过定义私有变量和对应的set方法可以帮助我们做参数检查，避免传入无效的参数。
-# （6）是否存在私有方法？self.__private_method
+# （6）是否存在私有方法？self.__private_method 私有方法和私有变量类似，不能通过外部调用。
 
 # 实例5-1 调用构造方法实例变量，并在类的外部进行修改
 '''
@@ -61,6 +61,7 @@ print("修改之后的分数是：",st.get_score())
 st.info()
 '''
 # 实例5-4 通过set做参数检查-输入无效参数
+'''
 class Student:
     def __init__(self,name,score):
         self.__name=name
@@ -73,12 +74,24 @@ class Student:
         if 0<=score<=100:
             self.__score = score
         else:
-            print("")
-
-
+            print("请输入1到100的数字")
 st=Student("jackson",99.9)
 print("修改之前的分数是：",st.get_score())  #在累的外部访问私有变量，通过get方法
 st.info()
 st.set_score(-10)  #在类的外部修改私有变量，通过set方法
 print("修改之后的分数是：",st.get_score())
 st.info()
+'''
+# 实例5-5 私有方法
+class Student:
+    def __init__(self):
+        pass
+    def __info(self):
+        print("---这是一个私有方法---")
+    def info(self):
+        print("---这是一个公共方法---")
+        print("---在公共方法中调用私有方法---")
+        self.__info()
+st=Student()
+st.info()
+# st.__info()  #AttributeError: 'Student' object has no attribute '__info'
